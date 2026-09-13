ff7ed20379697a958ffc31c04d1c56d2f568d57c
osmeyerc7275-cmyk <osmeyerc7275@student.faytechcc.edu>
2026-09-12 22:52:46 -0400

    First python assignment
    Taking input from user and printing output

---
+++ P1LAB1_OsmeyerClaire.py
@@ -0,0 +1,8 @@
+# Claire Osmeyer
+# P1lab1.py
+# input and print 
+
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+
+print("Hello, " + first + " " + last + "!", "Welcome to CTI-110!")
